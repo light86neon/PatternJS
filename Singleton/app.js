7e4086d63d79ea283ebe5ddb4d829d7bf30dfd6c
light86neon <72137342+light86neon@users.noreply.github.com>
2021-03-16 01:24:25 +0200

    first commit

---
+++ Singleton/app.js
@@ -0,0 +1,32 @@
+//singleton
+// маєм модуль counterModule, в середині маєм counter зі значенням 0, та instance.
+// кожного разу коли ми викликаємо increaseCounter наш лічильник інкрементує,
+// getCounter ми використовуємо щоб отрмати значення нашого лічильника. Ф-ція
+// createInstance викликається самий перший раз коли instance ще не існує, а саме в
+// нас створюється такий обєкт з ссилками на getCounter та increaseCounter, якщо
+// instance вже є в такому разі повертається instance;
+let counterModule = ( function () {
+    let counter = 0,
+        instance;
+
+    let getCounter = function () {
+        return counter;
+    }
+
+    let increaseCounter = function () {
+        counter++;
+    }
+
+    let createInstance = function () {
+        return {
+            getCounter: getCounter,
+            increaseCounter: increaseCounter()
+        }
+    }
+
+    return {
+        getInstance: function () {
+            return instance || (instance = createInstance());
+        }
+    }
+}());
