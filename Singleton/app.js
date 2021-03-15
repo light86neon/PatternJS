4fba120563856d2f23836756f4ccc630a33573ca
light86neon <72137342+light86neon@users.noreply.github.com>
2021-03-16 01:26:40 +0200

    Singleton

--- Singleton/app.js
+++ Singleton/app.js
@@ -5,28 +5,29 @@
 // createInstance викликається самий перший раз коли instance ще не існує, а саме в
 // нас створюється такий обєкт з ссилками на getCounter та increaseCounter, якщо
 // instance вже є в такому разі повертається instance;
+
 let counterModule = ( function () {
     let counter = 0,
         instance;
 
     let getCounter = function () {
         return counter;
-    }
+    };
 
     let increaseCounter = function () {
         counter++;
-    }
+    };
 
     let createInstance = function () {
         return {
             getCounter: getCounter,
             increaseCounter: increaseCounter()
         }
-    }
+    };
 
     return {
         getInstance: function () {
             return instance || (instance = createInstance());
         }
-    }
+    };
 }());
